7946aaffd01fc24ed9605513a5692dec99f15a75
Chemmamchaimaa <56743080+Chemmamchaimaa@users.noreply.github.com>
2019-10-23 20:09:51 +0200

    create database
    hello bla bla bla bla

---
+++ database.sql
@@ -0,0 +1,111 @@
+USE [master]
+GO
+
+/****** Object:  Database [gestion_etudiant]    Script Date: 23/10/2019 20:08:11 ******/
+CREATE DATABASE [gestion_etudiant]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'gestion_etudiant', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL11.SQLEXPRESS\MSSQL\DATA\gestion_etudiant.mdf' , SIZE = 4160KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'gestion_etudiant_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL11.SQLEXPRESS\MSSQL\DATA\gestion_etudiant_log.ldf' , SIZE = 1040KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [gestion_etudiant] SET COMPATIBILITY_LEVEL = 110
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [gestion_etudiant].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [gestion_etudiant] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET AUTO_CLOSE ON 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET AUTO_CREATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET  ENABLE_BROKER 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET RECOVERY SIMPLE 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [gestion_etudiant] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [gestion_etudiant] SET  READ_WRITE 
+GO
+
